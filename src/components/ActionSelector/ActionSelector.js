0ac81d7344eb24ecc0ef57deaafebafdfd3dbf39
Dan Kuebrich <dan.kuebrich@gmail.com>
2018-04-07 12:08:02 -0700

    Add note about rendering and object allocation

--- src/components/ActionSelector/ActionSelector.js
+++ src/components/ActionSelector/ActionSelector.js
@@ -7,6 +7,7 @@ class ActionSelector extends React.Component {
   }
 
   render() {
+    /* TODO: would be more efficient to not create an arrow function obj every time rendered */
     return <Dropdown placeholder='Assignment?' selection
               options={this.props.employee.actions.map((a,i) => ({key: a, value: a, text: a}))}
               onChange={(e, {value}) => this.props.employee.setAction(value)}
